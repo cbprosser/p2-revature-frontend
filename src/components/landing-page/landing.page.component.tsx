c1218f194d7832a77b912e3eee2025372c905416
Matt <MattJArsenault82@gmail.com>
2019-08-10 22:46:37 -0400

    Fixed text issues on displayed card. Limited the number of cards displayed

--- src/components/landing-page/landing.page.component.tsx
+++ src/components/landing-page/landing.page.component.tsx
@@ -1,58 +1,85 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { IState, IAuthState } from '../../reducers';
+// import { IState, IAuthState } from '../../reducers';
 import { Button, Card, CardImg, CardTitle, CardText, CardColumns, CardSubtitle, CardBody } from 'reactstrap';
 
-interface ILandProps {
+// interface ILandProps {
 
-}
+// }
 
 interface ILandState {
+    manaTypes: any[]
     cards: any[]
 }
 
-export class LandingPageComponenet extends React.Component<ILandProps, ILandState> {
+export class LandingPageComponenet extends React.Component<{}, ILandState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
+            manaTypes: [],
             cards: []
         }
     }
 
     async componentDidMount() {
         this.getRandomCards();
-    } 
+        this.getManaTypes();
+    }
 
     getRandomCards = async () => {
-        const resp = await fetch("https://api.scryfall.com/cards?page=3", {
+        const resp = await fetch("https://api.scryfall.com/cards?page=4", {
         });
         const cardList = await resp.json();
+        const cl = this.state.cards;
+        console.log(cardList)
+        for(let i = 0; i < 25; i++){
+            cl.push(cardList.data[i]);
+            
+        }
         this.setState({
             ...this.state,
-            cards: cardList.data
+            cards: cl
         });
+    }
+
+    getManaTypes = async () => {
+        const resp = await fetch("https://api.scryfall.com/symbology", {
+        });
+        const mana = await resp.json();
+        this.setState({
+            ...this.state,
+            manaTypes: mana.data
+        });
+    }
+
+    setupMana = async (card: any) => {
+        console.log(card.mana_cost);
+        const resp = await fetch("http://api.scryfall.com/symbology/parse-mana?cost=" + card.mana_cost, {
+        });
+        const img = await resp.json();
+        console.log('returned img: ' + img);
+        return (
+            <CardSubtitle>{img.cost}</CardSubtitle> 
+        );
     }
 
     cardColumns = () => {
         const cards = this.state.cards;
         return (
             <CardColumns>
-                {cards.map(aCard =>
+                {cards.map(MTGcard  =>
 
-                    <Card key={"card" + aCard.name}>
-                        <CardImg top width="100%" src={aCard.image_uris.normal} alt="Card image cap" />
-                        <CardBody class="">
-                            <CardTitle>{aCard.name}</CardTitle>
-                            <CardSubtitle>{aCard.mana_cost}</CardSubtitle>
-                            <CardText>{aCard.flavor_text}</CardText>
+                    <Card key={MTGcard.id}>
+                        <CardImg top width="100%" src={MTGcard.image_uris.normal} alt="Card image cap" />
+
+                        <Card body inverse color="primary">
+                            <CardTitle>{MTGcard.name}</CardTitle>
+                            <CardSubtitle>{MTGcard.mana_cost}</CardSubtitle> 
+                            <CardText>{MTGcard.flavor_text}</CardText>
                             <Button>Button</Button>
-                        </CardBody>
-                        {/* <Card body inverse color="primary">
-                                <CardTitle>Special Title Treatment</CardTitle>
-                                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                                <Button color="secondary">Button</Button>
-                            </Card> */}
+                        </Card>
                     </Card>
                 )
                 }
@@ -62,18 +89,24 @@ export class LandingPageComponenet extends React.Component<ILandProps, ILandStat
 
     render() {
         const cards = this.state.cards;
-       
+
         // this.getRandomCards();
         return (
             <div>
-               {this.cardColumns()}
+                {this.cardColumns()}
             </div>
         );
     }// end of render()
+
+
 }
+
+
 
 const mapStateToProps = (state: ILandState) => ({
 
 })
 
 export default connect()(LandingPageComponenet);
+
+
